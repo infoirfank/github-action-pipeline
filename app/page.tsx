a34e35f5ed9e3d726244a377f7d33ea3e8acefa5
irfan-khalid-arhamsoft <122451390+irfan-khalid-arhamsoft@users.noreply.github.com>
2024-10-31 20:19:42 +0500

    edit page

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main>
-      <h1>Hello my first code</h1>
+      <h1>Hello, all users How are you?</h1>
       <Link href="/users">Users</Link>
     </main>
   )
